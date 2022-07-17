c55819bfda15aea7b32ef94df79c6a28fae90d52
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-07-17 07:23:10 +0300

    add more fields, and modifications to vehicle model

--- spares_tracker/vehicles/admin.py
+++ spares_tracker/vehicles/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from spares_tracker.vehicles.models import Vehicle
+from spares_tracker.vehicles.models import (
+    Vehicle, Country, VehicleMake, VehicleModel)
 # Register your models here.
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('country_of_registration', 'chasis_number', 'registration_year', 'registration_month',
+    list_display = ('number_plate', 'country_of_registration', 'chasis_number', 'registration_year', 'registration_month',
                     'manufacture_year', 'manufacture_month', 'vehicle_model', 'vehicle_model_code', 'engine_size',
-                    'exterior_color', 'fuel', 'transmission', 'body_type')
+                    'exterior_color', 'fuel', 'transmission', 'body_type', 'drive_train', 'steering')
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('country_name',)
+    
+@admin.register(VehicleMake)
+class VehicleMakeAdmin(admin.ModelAdmin):
+    list_display = ('vehicle_make_name',)
+    
+@admin.register(VehicleModel)
+class VehicleModelAdmin(admin.ModelAdmin):
+    list_display = ('vehicle_model_name',)

dd373dbfbde0f90905f4b006ba3ea3f4096ba3dd
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-03 19:27:51 +0300

    add create supplier service

---
+++ spares_tracker/suppliers/services.py
@@ -0,0 +1,23 @@
+
+
+from spares_tracker.spares_tracker.suppliers.models import Supplier
+
+
+def supplier_create(
+    *,
+    name,
+    email,
+    phone,
+    address
+) -> Supplier:
+    supplier = Supplier(
+        name=name,
+        email=email,
+        phone=phone,
+        address=address
+    )
+
+    supplier.full_clean()
+    supplier.save()
+
+    return supplier

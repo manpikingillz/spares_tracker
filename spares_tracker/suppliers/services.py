44f4621d14c6e2487cd7247c15182c390c04aa1a
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-03 21:09:59 +0300

    add supplier update endpoint

--- spares_tracker/suppliers/services.py
+++ spares_tracker/suppliers/services.py
@@ -1,7 +1,9 @@
-
-
+from django.db import transaction
 from spares_tracker.suppliers.models import Supplier
+from django.core.exceptions import ValidationError
+from spares_tracker.common.services import model_update
 
+SUPPLIER_INSTANCE_IS_NONE = f'You attempted updating a {Supplier.__name__} that does not exist!'
 
 def supplier_create(
     *,
@@ -21,3 +23,29 @@ def supplier_create(
     supplier.save()
 
     return supplier
+
+
+@transaction.atomic
+def supplier_update(*, supplier: Supplier, data) -> Supplier:
+    model_fields = list(vars(supplier).keys())
+    model_fields.remove('id')
+
+    for field in model_fields:
+        if field == '_state':
+            model_fields.remove('_state')
+
+        if field.endswith('_id'):
+            modified_field = field[:-3]
+            model_fields.remove(field)
+            model_fields.append(modified_field)
+
+    if not supplier:
+        raise ValidationError(SUPPLIER_INSTANCE_IS_NONE)
+
+    _supplier, has_updated = model_update(
+        instance=supplier,
+        fields=model_fields,
+        data=data
+    )
+
+    return _supplier

ea574bdf405e379e998a08778b18d3171dc4abd3
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-06 02:24:57 +0300

    Added models - Employee, Station, Division, Region

--- spares_tracker/employee/admin.py
+++ spares_tracker/employee/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from spares_tracker.employee.models import Division, Employee, Region, Station
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'middle_name', 'gender', 'email', 'phone_number', 'address', 'station')
+
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'division')
+
+
+@admin.register(Division)
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region')
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- spares_tracker/employee/models.py
+++ spares_tracker/employee/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from spares_tracker.common.models import BaseModel
+
+
+class Region(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Division(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+    region = models.ForeignKey(Region, related_name='divisions', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Station(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+    division = models.ForeignKey(Division, related_name='stations', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Employee(BaseModel):
+    class Gender(models.TextChoices):
+        FEMALE = 'FEMALE', 'Female'
+        MALE = 'MALE', 'Male'
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, null=True, blank=True)
+    gender =  models.CharField(choices=Gender.choices, max_length=6)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=30, unique=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    station =  models.ForeignKey(Station, related_name='employees', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.middle_name or " "} {self.last_name}'
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.middle_name or " "} {self.last_name}'

1c30e823fb40e7caac0dad947d18caf66990b9a2
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-06 15:02:00 +0300

    added SparePartsCategory model

--- spares_tracker/spareparts/models.py
+++ spares_tracker/spareparts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from spares_tracker.common.models import BaseModel
+from spares_tracker.files.models import File
+
 # Create your models here.
+class SparePartsCategory(BaseModel):
+    category_name = models.CharField(max_length=255, unique=True)
+    image = models.ForeignKey(
+        File,
+        related_name='sparepart_category_images',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    relates_to = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.category_name

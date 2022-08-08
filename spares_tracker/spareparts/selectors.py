b8e2511c8a8150d5ea7387807df7dc2485a83436
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-08 12:49:43 +0300

    added selectors - sparepart_purchase_list, sparepart_purchase_detail

--- spares_tracker/spareparts/selectors.py
+++ spares_tracker/spareparts/selectors.py
@@ -1,7 +1,7 @@
 from spares_tracker.common.utils import get_object
-from spares_tracker.spareparts.models import SparePart, SparePartCategory
+from spares_tracker.spareparts.models import SparePart, SparePartCategory, SparePartPurchase
 from django.db.models.query import QuerySet
-from spares_tracker.spareparts.filters import BaseSparePartFilter, BaseSparePartCategoryFilter
+from spares_tracker.spareparts.filters import BaseSparePartFilter, BaseSparePartCategoryFilter, BaseSparePartPurchaseFilter
 
 # spare parts selectors
 def sparepart_list(*, filters=None) -> QuerySet[SparePart]:
@@ -25,3 +25,14 @@ def sparepart_category_list(*, filters=None) -> QuerySet[SparePartCategory]:
     else:
         qs = SparePartCategory.objects.filter(removed=False)
     return BaseSparePartCategoryFilter(filters, qs).qs
+
+# spare parts purchases selectors
+def sparepart_purchase_list(*, filters=None) -> QuerySet[SparePartPurchase]:
+    filters = filters or {}
+
+    qs = SparePartPurchase.objects.filter(removed=False)
+    return BaseSparePartPurchaseFilter(filters, qs).qs
+
+
+def sparepart_purchase_detail(*, pk) -> SparePartPurchase:
+    return get_object(SparePartPurchase, pk=pk)

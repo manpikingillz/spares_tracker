bb140e0cffc4354824ce395c9461464be60dd478
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-09 02:08:23 +0300

    fix naming error in admin.py

--- spares_tracker/spareparts/admin.py
+++ spares_tracker/spareparts/admin.py
@@ -12,5 +12,5 @@ class SparePartAdmin(admin.ModelAdmin):
     list_display = ('name', 'code','quantity', 'price', 'image', 'barcode', 'category')
 
 @admin.register(SparePartPurchase)
-class SparePartPurchase(admin.ModelAdmin):
+class SparePartPurchaseAdmin(admin.ModelAdmin):
     list_display = ('spare_part', 'order_number', 'quantity', 'unit_price', 'amount_paid', 'supplied_by', 'received_by',)

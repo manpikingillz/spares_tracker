7e64981ebf970f5d4ad468b27e0d3545dd5c9015
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-08-08 13:44:12 +0300

    added endpoint - SparePartPurchaseCreateApi

--- spares_tracker/spareparts/apis.py
+++ spares_tracker/spareparts/apis.py
@@ -7,6 +7,7 @@ from spares_tracker.common.utils import inline_serializer
 from spares_tracker.spareparts.models import SparePart, SparePartCategory, SparePartPurchase
 from spares_tracker.spareparts.selectors import sparepart_category_list, sparepart_list, sparepart_purchase_detail, sparepart_purchase_list
 from spares_tracker.employee.models import Employee
+from spares_tracker.spares_tracker.spareparts.services import sparepart_purchase_create
 from spares_tracker.suppliers.models import Supplier
 from spares_tracker.vehicles.models import VehicleModel
 
@@ -70,17 +71,42 @@ class SparePartListApi(ApiAuthMixin, APIView):
         data = self.OutputSerializer(spareparts, many=True).data
         return Response(data, status=status.HTTP_200_OK)
 
+
 # spare part purchase endpoints
+class SparePartPurchaseCreateApi(ApiAuthMixin, APIView):
+    class InputSerializer(serializers.Serializer):
+        spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePart.objects.all(), required=True)
+        order_number = serializers.CharField(max_length=255, required=False)
+        quantity = serializers.IntegerField(required=True)
+        unit_price = serializers.DecimalField(max_digits=15, decimal_places=2, required=True)
+        amount_paid = serializers.DecimalField(max_digits=15, decimal_places=2, required=True)
+        supplied_by = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all(), required=True)
+        received_by = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), required=True)
+
+    class FilterSerializer(serializers.Serializer):
+        spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePart.objects.all(), required=False)
+        order_number = serializers.CharField(required=False)
+
+    def get(self, request):
+        # Make sure the filters are valid
+        filters_serializer = self.FilterSerializer(data=request.query_params)
+        filters_serializer.is_valid(raise_exception=True)
+
+        sparepart_purchases = sparepart_purchase_create(filters=filters_serializer.validated_data)
+
+        data = self.InputSerializer(sparepart_purchases, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
 class SparePartPurchaseListApi(ApiAuthMixin, APIView):
     class OutputSerializer(serializers.Serializer):
         id = serializers.IntegerField()
-        spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePartPurchase.objects.all())
-        order_number = serializers.CharField()
-        quantity = serializers.IntegerField()
-        unit_price = serializers.DecimalField(max_digits=15, decimal_places=2)
-        amount_paid = serializers.DecimalField(max_digits=15, decimal_places=2)
-        supplied_by = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all())
-        received_by = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
+        spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePart.objects.all(), required=True)
+        order_number = serializers.CharField(max_length=255, required=False)
+        quantity = serializers.IntegerField(required=True)
+        unit_price = serializers.DecimalField(max_digits=15, decimal_places=2, required=True)
+        amount_paid = serializers.DecimalField(max_digits=15, decimal_places=2, required=True)
+        supplied_by = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all(), required=True)
+        received_by = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), required=True)
 
     class FilterSerializer(serializers.Serializer):
         spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePart.objects.all(), required=False)
@@ -100,13 +126,14 @@ class SparePartPurchaseListApi(ApiAuthMixin, APIView):
 class SparePartPurchaseDetailApi(ApiAuthMixin, APIView):
     class OutputSerializer(serializers.Serializer):
         id = serializers.IntegerField()
-        spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePartPurchase.objects.all())
-        order_number = serializers.CharField()
-        quantity = serializers.IntegerField()
-        unit_price = serializers.DecimalField(max_digits=15, decimal_places=2)
-        amount_paid = serializers.DecimalField(max_digits=15, decimal_places=2)
-        supplied_by = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all())
-        received_by = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
+        spare_part = serializers.PrimaryKeyRelatedField(queryset=SparePart.objects.all(), required=True)
+        order_number = serializers.CharField(max_length=255, required=False)
+        quantity = serializers.IntegerField(required=True)
+        unit_price = serializers.DecimalField(max_digits=15, decimal_places=2, required=True)
+        amount_paid = serializers.DecimalField(max_digits=15, decimal_places=2, required=True)
+        supplied_by = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all(), required=True)
+        received_by = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all(), required=True)
+
 
     def get(self, request, sparepart_purchase_id):
         sparepart_purchase = sparepart_purchase_detail(pk=sparepart_purchase_id)
